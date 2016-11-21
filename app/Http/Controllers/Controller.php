8a9b22a0d48cc61671a6dd211b77020ab145e66e
tyb917 <461670819@qq.com>
2016-11-21 14:19:11 +0800

    BaseController 控制器修改

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as Controller;
+use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Germey\Geetest\CaptchaGeetest;
 
-class Controller extends Controller
+class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, CaptchaGeetest;
 }
