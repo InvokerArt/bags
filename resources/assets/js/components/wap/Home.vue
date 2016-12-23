7a4c6dfb8eab1770e11f63e6ed7c4dcb143f7eff
tyb917 <461670819@qq.com>
2016-12-23 10:51:05 +0800

    修改wap主页样式

--- resources/assets/js/components/wap/Home.vue
+++ resources/assets/js/components/wap/Home.vue
@@ -2,6 +2,9 @@
     <div>
         <section class="content homepage">
             <div class="logo"><img src="/images/logo.png" alt=""></div>
+            <h3>下载App，环保降解袋</h3>
+            <p>让绿色产品B2C</p>
+            <p>对我们共同的地球负责</p>
             <div class="banner"><img src="/images/wap/xuanchuan-banner.png" alt=""></div>
             <footer>
                 <ul>
