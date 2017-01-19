eb3d5dcec37ed51b8796d74237bb2e45c259dbbc
木乃伊会跳舞 <871972408@qq.com>
2017-01-19 10:42:28 +0800

    增加评论表用户外键

---
+++ database/migrations/2017_01_19_100744_add_replies_table_foreign.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRepliesTableForeign extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('replies', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
